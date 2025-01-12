c51f2781cc50819b44fb5c3069743a4675e7a614
BillyLOLwkwk <atanasiusbrilliant@gmail.com>
2025-01-12 07:55:36 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#802929",
+        altBg: "#FEF9E1",
+        dark: "#0f172a",
+        secondary: "#64748b",
+        youtube: "#FF0000",
+        instagram: "#E4405F",
+        facebook: "#1877F2",
+        tiktok: "#000000",
+        home: "#F68315",
+        kontak: "#26A5E4",
+        anggota: "#809CC9",
+      },
+      screens: {
+        "2xl": "1320 px",
+      },
+    },
+  },
+  plugins: [],
+};
